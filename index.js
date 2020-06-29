32c7203dbc8007d0063d89a2f3797ae4a9201e1d
SimplyJonas <43334964+SimplyJonas@users.noreply.github.com>
2020-06-29 22:31:28 +0200

    Added rank role

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 require('dotenv').config()
 
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const client = new Discord.Client({ partials: ['MESSAGE', 'REACTION'] });
+const rankRoles = [
+    'Iron',
+    'Bronze',
+    'Silver',
+    'Gold',
+    'Platinum',
+    'Diamond',
+    'Immortal',
+    'Radiant'
+];
 
 var creatorId = process.env.CREATOR_ID;
 var creatorName = process.env.CREATOR_NAME;
+var reactRankId = process.env.REACTION_ID;
 var channels = [];
 
 client.on('ready', () => {
@@ -34,18 +45,50 @@ client.on('voiceStateUpdate', (oldState, newState) => {
 
     // Delete empty spawned channels
     try {
-            let channelIndex = channels.indexOf(oldState.channelID);
+        let channelIndex = channels.indexOf(oldState.channelID);
 
-            // If channel exist and is empty, delete it
-            if((channelIndex !== -1) && (oldState.channel.members.size === 0)) {
-                oldState.channel.delete()
-                .then(() => {
-                    delete channels[channelIndex];
-                })
-                .catch(console.error)
-            }
+        // If channel exist and is empty, delete it
+        if((channelIndex !== -1) && (oldState.channel.members.size === 0)) {
+            oldState.channel.delete()
+            .then(() => {
+                delete channels[channelIndex];
+            })
+            .catch(console.error)
+        }
     } catch (error) {
         console.log(error)
+    }
+});
+
+client.on('messageReactionAdd', async  (reaction, user) => {
+    // If reaction is partial, try to fetch it (used for old messages)
+    if(reaction.partial) {
+        try {
+            await reaction.fetch();
+        } catch (error) {
+            console.log(error);
+            return;
+        }
+    }
+
+    // Check if reacted to the correct message
+    if(reaction.message.id === reactRankId) {
+        // Find  the react role in the guild
+        let guildRole = reaction.message.guild.roles.cache.find(role => role.name === reaction.emoji.name)
+        let roleManager = reaction.message.member.roles;
+    
+        if(guildRole) {
+            // Clear old rank roles
+            roleManager.cache.forEach(role => {
+                if(rankRoles.indexOf(role.name) !== -1) {
+                    roleManager.remove(role);
+                }
+            });
+        
+            // Add new role
+            roleManager.add(guildRole)
+            .catch(console.error);
+        }
     }
 });
 
